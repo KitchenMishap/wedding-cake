c16de0cc96f1f27de4cc3a4653a02ba0feebf60b
KitchenMishap <hello@kitchenmishap.art>
2026-07-26 15:07:46 +0100

    Prefix file path

---
+++ tierbake/prefixfilepath.go
@@ -0,0 +1,58 @@
+package tierbake
+
+import (
+	"fmt"
+
+	"github.com/kitchenmishap/wedding-cake/shallowtreebyte"
+)
+
+func CalculatePrefixPattern(prefixNibblesDigits byte, digitsPerFolder byte) (byte, []byte) {
+	remainingDigits := prefixNibblesDigits
+
+	// As a first step, we allocate up to digitsPerFolder digits to the filename
+	resultFilenameDigits := digitsPerFolder
+	if remainingDigits < digitsPerFolder {
+		resultFilenameDigits = remainingDigits
+	}
+	remainingDigits -= resultFilenameDigits
+
+	// Secondly, we add a few digits to each folder level until none ar left
+	resultFolderDigits := []byte{0} // To start with, one folder level is up for negotation
+	for remainingDigits > 0 {
+		nextLevelFolderDigits := digitsPerFolder
+		if remainingDigits < digitsPerFolder {
+			nextLevelFolderDigits = remainingDigits
+		}
+		resultFolderDigits[0] = nextLevelFolderDigits
+		remainingDigits -= nextLevelFolderDigits
+
+		if remainingDigits > 0 {
+			// Add a new folder level at the start for negotiation
+			resultFolderDigits = append([]byte{0}, resultFolderDigits...)
+		}
+	}
+
+	return resultFilenameDigits, resultFolderDigits
+}
+
+func formatNibble(nibble shallowtreebyte.NibbleVal) string {
+	return fmt.Sprintf("%01X", nibble)
+}
+
+func formatFilePathFilename(prefixNibbles []shallowtreebyte.NibbleVal, filenameDigits byte, folderNameDigits []byte) (string, string) {
+	length := len(prefixNibbles)
+	filename := ""
+	for digit := range int(filenameDigits) {
+		filename += formatNibble(prefixNibbles[length-2+digit])
+	}
+	folderName := ""
+	digit := 0
+	for folderNum := range len(folderNameDigits) {
+		for range folderNameDigits[folderNum] {
+			folderName += formatNibble(prefixNibbles[digit])
+			digit++
+		}
+		folderName += "/"
+	}
+	return filename, folderName
+}
---
+++ tierbake/prefixfilepath_test.go
@@ -0,0 +1,83 @@
+package tierbake
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/kitchenmishap/wedding-cake/shallowtreebyte"
+)
+
+func TestCalculatePrefixPattern(t *testing.T) {
+	const digitsPerFolder = byte(2)
+	file, folders := CalculatePrefixPattern(0, digitsPerFolder)
+	if file != 0 {
+		t.Fatal("Expected file digits to be 0")
+	}
+	if !bytes.Equal(folders, []byte{0}) {
+		t.Fatal("Expected folder digits to be a single 0")
+	}
+
+	file, folders = CalculatePrefixPattern(1, digitsPerFolder)
+	if file != 1 {
+		t.Fatal("Expected file digits to be 1")
+	}
+	if !bytes.Equal(folders, []byte{0}) {
+		t.Fatal("Expected folder digits to be a single 0")
+	}
+
+	file, folders = CalculatePrefixPattern(2, digitsPerFolder)
+	if file != 2 {
+		t.Fatal("Expected file digits to be 2")
+	}
+	if !bytes.Equal(folders, []byte{0}) {
+		t.Fatal("Expected folder digits to be a single 0")
+	}
+
+	file, folders = CalculatePrefixPattern(3, digitsPerFolder)
+	if file != 2 {
+		t.Fatal("Expected file digits to be 2")
+	}
+	if !bytes.Equal(folders, []byte{1}) {
+		t.Fatal("Expected folder digits to be a single 1")
+	}
+
+	file, folders = CalculatePrefixPattern(4, digitsPerFolder)
+	if file != 2 {
+		t.Fatal("Expected file digits to be 2")
+	}
+	if !bytes.Equal(folders, []byte{2}) {
+		t.Fatal("Expected folder digits to be a single 2")
+	}
+
+	file, folders = CalculatePrefixPattern(5, digitsPerFolder)
+	if file != 2 {
+		t.Fatal("Expected file digits to be 2")
+	}
+	if !bytes.Equal(folders, []byte{1, 2}) {
+		t.Fatal("Expected folder digits to be (1,2)")
+	}
+}
+
+func TestFormatFilePathFilename(t *testing.T) {
+	const digitsPerFolder = byte(2)
+	nibbleCount := byte(3)
+	filenameDigits, foldersDigits := CalculatePrefixPattern(nibbleCount, digitsPerFolder)
+	filename, folderName := formatFilePathFilename([]shallowtreebyte.NibbleVal{0, 1, 2}, filenameDigits, foldersDigits)
+	if filename != "12" {
+		t.Fatal("Expected filename to be 12")
+	}
+	if folderName != "0/" {
+		t.Fatal("Expected folderName to be 0/")
+	}
+
+	nibbleCount = byte(5)
+	filenameDigits, foldersDigits = CalculatePrefixPattern(nibbleCount, digitsPerFolder)
+	filename, folderName = formatFilePathFilename([]shallowtreebyte.NibbleVal{1, 2, 3, 4, 5}, filenameDigits, foldersDigits)
+	if filename != "45" {
+		t.Fatal("Expected filename to be 45")
+	}
+	if folderName != "1/23/" {
+		t.Fatal("Expected folderName to be 1/23/")
+	}
+
+}
